30e30df45789f21ca9c234f0690dc1051d2fbd4b
Caitlin Swickard <caitlin6029@gmail.com>
2024-01-17 15:58:11 -0700

    style active link in navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,10 +1,15 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 import { AiFillBug } from "react-icons/ai";
+import classnames from "classnames";
 
 const NavBar = () => {
-  // dynamic links for nav bar
+  const currentPath = usePathname(); //this hook is dependent on browser APIs
+
   const links = [
+    // dynamic links for nav bar
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
   ];
@@ -19,7 +24,11 @@ const NavBar = () => {
         {links.map((link) => (
           <Link
             key={link.href}
-            className="text-zinc-500 hover:text-zinc-800 transition-colors"
+            className={classnames({
+              "text-zinc-900": link.href === currentPath,
+              "text-zinc-500": link.href !== currentPath,
+              "hover:text-zinc-800 transition-colors": true,
+            })}
             href={link.href}
           >
             {link.label}
